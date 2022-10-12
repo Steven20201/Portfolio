d5115d22073bdeccc9c34edf435d41227e02ada0
Steven20201 <khantzawhein261@example.com>
2022-10-12 16:02:14 +0630

    Master

--- assets/js/script.js
+++ assets/js/script.js
@@ -70,41 +70,6 @@ document.addEventListener('visibilitychange',
     });
 
 
-// <!-- typed js effect starts 
-var typed = new Typed(".typing-text", {
-    strings: ["frontend development", "backend development", "web designing", "android development", "web development"],
-    loop: true,
-    typeSpeed: 50,
-    backSpeed: 25,
-    backDelay: 500,
-});
-// <!-- typed js effect ends -->
-
-async function fetchData(type = "skills") {
-    let response
-    type === "skills" ?
-        response = await fetch("skills.json")
-        :
-        response = await fetch("./projects/projects.json")
-    const data = await response.json();
-    return data;
-}
-
-function showSkills(skills) {
-    let skillsContainer = document.getElementById("skillsContainer");
-    let skillHTML = "";
-    skills.forEach(skill => {
-        skillHTML += `
-        <div class="bar">
-              <div class="info">
-                <img src=${skill.icon} alt="skill" />
-                <span>${skill.name}</span>
-              </div>
-            </div>`
-    });
-    skillsContainer.innerHTML = skillHTML;
-}
-
 function showProjects(projects) {
     let projectsContainer = document.querySelector("#work .box-container");
     let projectHTML = "";
@@ -146,10 +111,6 @@ function showProjects(projects) {
     srtop.reveal('.work .box', { interval: 200 });
 
 }
-
-fetchData().then(data => {
-    showSkills(data);
-});
 
 fetchData("projects").then(data => {
     showProjects(data);
